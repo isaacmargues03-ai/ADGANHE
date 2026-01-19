ceb492ffbd97ae6b5148ee119abac5b4e5fc886c
isaac avakin 2020 <djbruuu1@gmail.com>
2026-01-19 17:15:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6055706558-caf46",
+  "appId": "1:432325739668:web:7a04aae8d060e728dc2081",
+  "apiKey": "AIzaSyD5nwrin6EE8_ej7WRlNckIxj4Dn9ID4E4",
+  "authDomain": "studio-6055706558-caf46.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "432325739668"
+};
