09710b8fc36b279b2aa6829e1fb33855ee4d5493
isaac avakin 2020 <djbruuu1@gmail.com>
2026-01-19 22:33:49 +0000

    N FUNCIONA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
             gtag('config', 'G-1234567890');
           `}
         </Script>
-        <script src="https://pl28480612.effectivegatecpm.com/31/82/6a/31826a023155a801f37c5ba2389004c5.js"></script>
       </head>
       <body className="font-body antialiased">
         <FirebaseClientProvider>
